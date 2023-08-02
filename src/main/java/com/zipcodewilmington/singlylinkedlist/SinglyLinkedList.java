924584a8aa01f7057dbd68b5c1a6fdd6636d19d5
olamide <oadams@fandm.edu>
2023-08-02 10:30:18 -0400

    implemented interface

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,42 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList implements LinkedListIface {
+
+    public SinglyLinkedList() {}
+    public void add(Object data) {
+
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException {
+
+    }
+
+    public Object get(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    public boolean contains(Object data) {
+        return false;
+    }
+
+    public int find(Object data) {
+        return 0;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public SinglyLinkedList copy() {
+        return null;
+    }
+
+    public void sort(Comparator comparator) {
+
+    }
 }
